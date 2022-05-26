37b09df2adc549ae99e3130b62f620f5e50f5068
josedm64 <trenodm@gmail.com>
2022-05-26 11:22:23 +0200

    Adjunto el avance tras el día 1
    Esto es todo lo que avanzamos respecto a programación el día 1.

--- Practica3/main.c
+++ Practica3/main.c
@@ -22,117 +22,149 @@
          \/____/                  ~~                       \|___|                   \/____/
 
    Programa de ejemplo para la AureBoard
-   Requerido: Aureboard Versión>2.0
+   Requerido: Aureboard Versiï¿½n>2.0
 
    - El programa configura el puerto Serie por USB.
-   - Cada programa deberá ir en su carpeta al mismo nivel de la carpeta lib
+   - Cada programa deberï¿½ ir en su carpeta al mismo nivel de la carpeta lib
+
+    A.U.R.E. 2010
+*/
 
 
 
 
-   A.U.R.E. 2010
+/*
+//NO ENCUENTRA LAS LIBRERIAS
+#include <../lib/sonido.h>
+#include <../lib/2leds.h>
 */
 
 #include <../lib/aurebot.h>
 #include <../lib/motores.h>
 #include <../lib/flex_lcd.c>
-/*#include <../lib/ldr.h>
+#include <../lib/ldr.h>
 #include <../lib/cny70.h>
 #include <../lib/bumper.h>
-#include <../lib/sonido.h>
-#include <../lib/2leds.h>*/
 
+// Rutina de gestion de pulsaciones serie
+void aure_serie(){
+    output_toggle(LED);
 
-
-
-
-// Rutina de gestión de pulsaciones serie
-void aure_serie()
-{
-   output_toggle(LED);
-   switch(keypress)
-   {
-//Control de movimiento
-            case 'w':
-               motores_palante();
-               break;
-            case 'W':
-               motores_palante();
-               break;
-            case 's':
-               motores_patras();
-               break;
-            case 'S':
-               motores_patras();
-               break;
-            case 'd':
-               motores_paderecha();
-               break;
-            case 'D':
-               motores_paderecha();
-               break;
-            case 'a':
-               motores_paizda();
-               break;
-            case 'A':
-               motores_paizda();
-               break;
-            case 'q':
-               motores_parar();
-               break;
-            case 'Q':
-               motores_parar();
-               break;               
-            default:
-               printf (usb_cdc_putc, "NR:%c\r\n", keypress);
-               break;
-   }
-   //Vaciamos el buffer
-   keypress=0;
+    switch(keypress){    //Control de movimiento
+        case 'w':
+            motores_palante();
+            break;
+        
+        case 'W':
+            motores_palante();
+            break;
+      
+        case 's':
+            motores_patras();
+            break;
+      
+        case 'S':
+            motores_patras();
+            break;
+            
+        case 'd':
+            motores_paderecha();
+            break;
+            
+        case 'D':
+            motores_paderecha();
+            break;
+      
+        case 'a':
+            motores_paizda();
+            break;
+      
+        case 'A':
+            motores_paizda();
+            break;
+      
+        case 'q':
+            motores_parar();
+            break;
+      
+        case 'Q':
+            motores_parar();
+            break;
+            
+        default:
+            printf (usb_cdc_putc, "NR:%c\r\n", keypress);
+            break;
+    }
+   
+    //Vaciamos el buffer
+    keypress=0;
 }
 
-
-
 //Rutina principal
-void main()
-{
-   int i, d, j, k;
-   aure_configurar();
-   lcd_configurar();
-   aure_configurar_usb_sinespera();
-   
-   for(i=0; i<50; i++)
-   {
-     delay_ms(100);
-     if(input(PULSADOR))i=100;
+void main(){
+    //DeclaraciÃ³n de variables
+    int LDR_1, LDR_2;
+    int i, d, j, k;
+
+    //DeclaraciÃ³n de configuraciones
+    aure_configurar();
+    lcd_configurar();
+    aure_configurar_usb_sinespera();
+
+    for(i=0; i<50; i++){
+        delay_ms(100);
+
+        if(input(PULSADOR))
+            i=100;
    }
-   
-   if (aure_configurar_usb_comprobar()==1) 
-   {
-   
-      // Si está conectado
-         while (!input(PULSADOR)) {aure_usb();} //Esperamos hasta que se pulse el pulsador 
-         aure_usb();
-         while (1) {aure_usb();}
-   } else {
-      // Si no está conectado   
-         while (!input(PULSADOR)) {} //Esperamos hasta que se pulse el pulsador 
-         motores_palante();
-         delay_ms(10000);
-         motores_parar();
-         motores_paderecha();
-         delay_ms(1000);
-         motores_parar();
-         motores_palante();
-         delay_ms(2000);
-         motores_parar();
-         motores_paizda();
-         delay_ms(1000);
-         motores_parar();
-         motores_palante();
-         delay_ms(8000);
-         motores_parar();
-   }
-   
-   while(1) {usb_task();}
+
+    if (aure_configurar_usb_comprobar()==1){
+        // Si esta conectado
+        while (!input(PULSADOR)){
+            aure_usb();  //Esperamos hasta que se pulse el pulsador
+        }
+
+        aure_usb();
+
+        while (1){
+            aure_usb();
+        }
+
+
+    } else{
+        //Si no esta conectado
+        while(!input(PULSADOR)){
+            //Esperamos hasta que se presione el pulsador
+        }
+
+        /*
+        //MOVIMIENTOS DEL ROBOT (DESHABILITADO TEMPORALMENTE PARA HACER PRUEBAS)
+        motores_palante();
+        delay_ms(10000);
+        motores_parar();
+
+        motores_paderecha();
+        delay_ms(1000);
+        motores_parar();
+        */
+
+        Output_high(PIN_A0); //LEDs del parachoque encendidos
+
+        //Variables con los valores en tiempo real de los LDRs
+        LDR_1 = Input(PIN_A1);
+        LDR_2 = Input(PIN_A2);
+
+        /*
+        if(LDR_1 > 0 || LDR_2 > 0)
+            Output_toggle(PIN_A0);
+        */
+        
+        //Funcionamiento de la LCD
+        lcd_init();
+        printf(LDR_1);
+
+        while(1){
+            usb_task();
+        }
+    }
 }
